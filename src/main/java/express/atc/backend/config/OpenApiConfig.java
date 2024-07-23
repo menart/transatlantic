dd0f0bc129874d3cfec2aeae2a3900b060c09f71
Artamonov Aleksandr <artamonov.a@alkor.co.ru>
2024-07-23 23:28:36 +0500

    add update user info

--- src/main/java/express/atc/backend/config/OpenApiConfig.java
+++ src/main/java/express/atc/backend/config/OpenApiConfig.java
@@ -18,13 +18,7 @@ public class OpenApiConfig {
         return new OpenAPI().addSecurityItem(new SecurityRequirement().
                         addList("Bearer Authentication"))
                 .components(new Components().addSecuritySchemes
-                        ("Bearer Authentication", createAPIKeyScheme()))
-                .info(new Info().title("My REST API")
-                        .description("Some custom description of API.")
-                        .version("1.0").contact(new Contact().name("Sallo Szrajbman")
-                                .email( "www.baeldung.com").url("salloszraj@gmail.com"))
-                        .license(new License().name("License of API")
-                                .url("API license URL")));
+                        ("Bearer Authentication", createAPIKeyScheme()));
     }
 
     private SecurityScheme createAPIKeyScheme() {
